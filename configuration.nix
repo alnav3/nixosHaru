fed690f8966eba26eae9d0a046999b1d278b492a
Adrián Estévez Rodríguez <48678376+adrestrod@users.noreply.github.com>
2024-12-16 20:39:09 +0100

    fix config

--- configuration.nix
+++ configuration.nix
@@ -6,7 +6,7 @@
 }: {
     security.polkit.enable = true;
     # Add overlays
-    nixpkgs = {
+  nixpkgs = {
     # You can add overlays here
     overlays = [
       # Add overlays your own flake exports (from overlays and pkgs dir):
